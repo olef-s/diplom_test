7bf05db87c2f8f69f5fd7f51409c627b82d93653
Ole Faarkrog-Søgaard <olex9008@skivecollege.dk>
2022-10-19 19:11:09 +0200

    Update app.js
    using of template string plus others

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ function SayHello(message){
 //path module
 const path = require('path');
 var pathobj = path.parse(__filename);
-console.log(pathobj);
+//console.log(pathobj);
 
 //os module
 const os = require('os');
-var totalmem = os.totalmem();
+var totalmem = os.totalmem(); 
 var freeMem = os.freemem();
-console.log('the amount of mem is '+totalmem+' total of free mem is '+freeMem);
+//console.log('the amount of mem is '+totalmem+' total of free mem is '+freeMem);
+//Template string - ES6 or ES2015: ECMAScript 6
+console.log(`Total memory ${totalmem}`);
