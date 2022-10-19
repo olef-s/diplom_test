f724134f065dd309056f0d806f538bf14bc913ee
Ole Faarkrog-Søgaard <Ofs1302@gmail.com>
2022-10-19 14:51:37 +0200

    path module

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ function SayHello(message){
 SayHello('hello from a function');
 SayHello('dette er endnu en test');
 SayHello('så er der endnu en ændring');
+const path = require('path');
+var pathobj = path.parse(__filename);
+console.log(pathobj);
