5f7e854eaef6e7a4e994c6c8b6474b15d1c23567
Ole Faarkrog-Søgaard <Ofs1302@gmail.com>
2022-10-19 20:01:03 +0200

    implement file system

--- app.js
+++ app.js
@@ -20,4 +20,19 @@ var totalmem = os.totalmem();
 var freeMem = os.freemem();
 //console.log('the amount of mem is '+totalmem+' total of free mem is '+freeMem);
 //Template string - ES6 or ES2015: ECMAScript 6
+
 console.log(`Total memory ${totalmem}`);
+
+//File systems
+const fs = require('fs');
+//See all the files in a directory called by a synchronized call
+const files = fs.readdirSync('./');
+console.log('files from sync call '+files);
+
+fs.readdir('./',function(err,files){
+    if (err) {
+        console.log('result from async call '+err);
+    } else {
+        console.log('result from async call '+files);
+    }
+});
