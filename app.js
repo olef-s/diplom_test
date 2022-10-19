896b6970cae97ef279738a6678da70016bffea6d
Ole Faarkrog-Søgaard <Ofs1302@gmail.com>
2022-10-19 20:12:34 +0200

    event mode part one

--- app.js
+++ app.js
@@ -21,18 +21,28 @@ var freeMem = os.freemem();
 //console.log('the amount of mem is '+totalmem+' total of free mem is '+freeMem);
 //Template string - ES6 or ES2015: ECMAScript 6
 
-console.log(`Total memory ${totalmem}`);
+//console.log(`Total memory ${totalmem}`);
 
 //File systems
-const fs = require('fs');
+//const fs = require('fs');
 //See all the files in a directory called by a synchronized call
-const files = fs.readdirSync('./');
-console.log('files from sync call '+files);
+//const files = fs.readdirSync('./');
+//console.log('files from sync call '+files);
 
-fs.readdir('./',function(err,files){
-    if (err) {
-        console.log('result from async call '+err);
-    } else {
-        console.log('result from async call '+files);
-    }
-});
+//fs.readdir('./',function(err,files){
+//    if (err) {
+//        console.log('result from async call '+err);
+//    } else {
+//        console.log('result from async call '+files);
+//    }
+//});
+
+//event module - a signal that something has occured
+const EventMitter = require('events');//class hence capitale letter
+const emitter = new EventMitter(); //object
+
+//register a listner
+emitter.on('messageLogged',function(){console.log('listner called');})
+
+//raise an event
+emitter.emit('messageLogged');
