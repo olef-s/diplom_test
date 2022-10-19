1d2c0eb490fbd92610f1225125a2d7aaf0ba6f97
Ole Faarkrog-Søgaard <Ofs1302@gmail.com>
2022-10-19 14:57:02 +0200

    os module incorp

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ function SayHello(message){
     console.log(message);
 }
 
-SayHello('hello from a function');
-SayHello('dette er endnu en test');
-SayHello('så er der endnu en ændring');
+//SayHello('hello from a function');
+//SayHello('dette er endnu en test');
+//SayHello('så er der endnu en ændring');
+
+//path module
 const path = require('path');
 var pathobj = path.parse(__filename);
 console.log(pathobj);
+
+//os module
+const os = require('os');
+var totalmem = os.totalmem();
+var freeMem = os.freemem();
+console.log('the amount of mem is '+totalmem+' total of free mem is '+freeMem);
