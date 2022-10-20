d37c65bc1552cd18a212fff86b9151ce9f0ca267
Ole Faarkrog-Søgaard <Ofs1302@gmail.com>
2022-10-20 10:40:59 +0200

    micro change to test nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app =express();
 
 app.get('/',(req, res)=>{
-res.send('Hello World you have entered port 3000...');
+res.send('Hello World you have entered port 3000 it works...');
 
 
 })
